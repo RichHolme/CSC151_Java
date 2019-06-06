ca7db8208835ee2541b2cd20d2a2f795c88304c5
Richard T Holme <holmert@mw.na.cat.com>
2019-06-06 12:50:44 -0400

    java printer

---
+++ JavaPrinter.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class JavaPrinter {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Want to see something cool? (y/n)");
+		String userString = input.nextLine();
+		
+		if(userString.equals("y")){
+
+			System.out.println("( \\");
+			System.out.println(" \\ \\");
+			System.out.println(" / /                |\\\\");
+			System.out.println("/ /     .-`````-.   / ^`-.");
+			System.out.println("\\ \\    /         \\_/ {|}  `o");
+			System.out.println(" \\ \\  /   .- -.   \\\\ _  ,--");
+			System.out.println("  \\ \\/   /     \\,  \\( `^^^");
+			System.out.println("   \\   \\/\\      (\\  )");
+			System.out.println("    \\   ) \\     ) \\ \\");
+			System.out.println("     ) /__ \\__  ) (\\ \\___");
+			System.out.println("    (___)))__))(__))(__)))");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("      /\\");
+			System.out.println("     |==|");
+			System.out.println("     |  |");
+			System.out.println("     |  |");
+			System.out.println("     |  |");
+			System.out.println("    /____\\");
+			System.out.println("    |    |");
+		    System.out.println("    |    |");
+			System.out.println("    |    |");
+			System.out.println("    |    |");
+			System.out.println("   /| |  |\\");
+			System.out.println("  / | |  | \\");
+			System.out.println(" /__|_|__|__\\");
+			System.out.println("    /_\\/_\\");
+			System.out.println("    ######");
+			System.out.println("   ########");
+			System.out.println("    ######");
+			System.out.println("     ####");
+			System.out.println("     ####");
+		    System.out.println("      ##");
+		    System.out.println("      ##");
+		    System.out.println("      ##");
+		    System.out.println("      ##");
+				
+		}else{
+			System.out.println(" __        ______       _______ _______ ______");   
+			System.out.println("|  |      /  __  \\     /       |   ____|   _  \\");     
+			System.out.println("|  |     |  |  |  |   |   (----|  |__  |  |_)  |");    
+			System.out.println("|  |     |  |  |  |    \\   \\   |   __| |      /");     
+			System.out.println("|  `----.|  `--'  |.----)   |  |  |____|  |\\  \\----.");
+			System.out.println("|_______| \\______/ |_______/   |_______| _| `._____|");
+		}
+	}
+
+}
